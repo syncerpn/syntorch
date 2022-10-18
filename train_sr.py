57438180b1ecdec86166783ee709167f5151f957
syncerpn <nghianteeek57@gmail.com>
2022-10-18 14:26:54 +0900

    update gradnorm

--- train_sr.py
+++ train_sr.py
@@ -40,6 +40,11 @@ def train(epoch):
         batch_loss = loss_func(yf, yt)
         optim.zero_grad()
         batch_loss.backward()
+        #nghiant: 2022/10/18 integrate grad norm into training
+        if args.gradnorm > 0:
+            utils.normalize_grad_(all_params, args.gradnorm)
+        #nghiant: end
+
         optim.step()
 
         total_loss += batch_loss
