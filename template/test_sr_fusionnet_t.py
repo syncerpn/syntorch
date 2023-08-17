01c7bfffbcb087f2572264fa9f63bd3441f183b8
syncerpn <nghianteeek57@gmail.com>
2023-08-17 23:08:18 +0900

    Update test_sr_fusionnet_t.py

--- template/test_sr_fusionnet_t.py
+++ template/test_sr_fusionnet_t.py
@@ -29,5 +29,14 @@ def set_template(args):
 			args.scale=2
 			args.core="FusionNet_7_3s"
 
+		elif args.template == "FusionNet_7_4s_1":
+			print(f"[INFO] Template found: {args.template}")
+			args.style="Y"
+			args.testset_tag="Set14B"
+			args.testset_dir="/home/dataset/set5_dnb/2x/"
+			args.rgb_range=1.0
+			args.scale=2
+			args.core="FusionNet_7_4s"
+
 		else:
 			assert 0, f"[ERRO] Template not found {args.template}"
