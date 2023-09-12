a3f5cf75ecc391c43fbb597849cdc38035ddb5c6
syncerpn <nghianteeek57@gmail.com>
2023-09-12 20:57:22 +0900

    Update mask_generator_lib.py

--- mask_generator_lib.py
+++ mask_generator_lib.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 
 class GradientSobelFilter:
-    def __init__(self):
-        self.sfx = nn.Conv2d(1, 1, 3, 1, 1, bias=False)
-        self.sfx.weight.data = torch.Tensor([[[[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]]])
+    def __init__(self, n_groups=1):
+        self.n_groups = n_groups
+
+        self.sfx = nn.Conv2d(1, 1, 3, 1, 1, bias=False, groups=self.n_groups)
+        self.sfx.weight.data = torch.Tensor([[[[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]]]).expand([self.n_groups,1,1,1,1])
         self.sfx.to('cuda')
 
-        self.sfy = nn.Conv2d(1, 1, 3, 1, 1, bias=False)
-        self.sfy.weight.data = torch.Tensor([[[[-1, -2, -1], [0, 0, 0], [1, 2, 1]]]])
+        self.sfy = nn.Conv2d(1, 1, 3, 1, 1, bias=False, groups=self.n_groups)
+        self.sfy.weight.data = torch.Tensor([[[[-1, -2, -1], [0, 0, 0], [1, 2, 1]]]]).expand([self.n_groups,1,1,1,1])
         self.sfy.to('cuda')
 
     def generate_mask(self, x, p):
