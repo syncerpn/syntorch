d46a40855b4a600c36e5cd132e63b52d6ac22203
NGUYỄN MẠNH DƯƠNG <nmduong2104@gmail.com>
2023-09-26 16:26:45 +0700

    Update

--- train_SM_sr_S_kd.py
+++ train_SM_sr_S_kd.py
@@ -55,7 +55,8 @@ def train_teacher(epoch, optim):
         loss_sparsity = sparsity.mean() # we try to reduce the sparsity to perseve information from features
         lambda0 = 0.0
         # lambda_sparsity = min((epoch - 1) / 50, 1) * lambda0
-        lambda_sparsity = 0.0 if epoch < 40 else -0.001
+        # lambda_sparsity = 0.0 if epoch < 40 else -0.001
+        lambda_sparsity = 0.0
         batch_loss = loss_SR + lambda_sparsity * loss_sparsity
         
         optim.zero_grad()
